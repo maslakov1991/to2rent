4edeb7c6d2f77abcb36633a1beb77cd6fb679d4c
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-02-17 16:15:47 +0200

    search section done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
+$(function () {
+
+    $('.slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
+        fade: true,
+    });
 
 });
