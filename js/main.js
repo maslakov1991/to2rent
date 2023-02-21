72f405a39a82826292a6173a2c95b32a266fe4b5
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-02-21 14:57:53 +0200

    adaptive done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(function () {
         prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
         fade: true,
+        responsive: [
+            {
+                breakpoint: 421,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
     });
 
 });
